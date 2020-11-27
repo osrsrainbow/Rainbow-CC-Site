f7697c35c4dde4b918bbfc44f2e5c30ea66c59da
Taylor Abraham <taylor.abra8@gmail.com>
2020-11-27 18:18:44 -0500

    Add about us and flag banner

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,16 +4,7 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
@@ -24,26 +15,27 @@ class About extends Component {
          </div>
          <div className="nine columns main-col">
             <h2>About Us</h2>
+            <p><strong>OSRS CC Name:</strong> Rainbow CC</p>
+          <h3>Who are we?</h3>
+          <p>
+          We are a social LGBTQIA+ / Queer OSRS Clan that strongly believes in intersectionality and grassroots leadership, where our members have a real say in clan decisions that affect us all.
+          We strive to provide a safe space where everyone can feel comfortable being themselves.
+          </p>
+          <h3>Eligibility</h3>
+          <p>Open to all LGBT / Queer players and respectful Allies</p>
+          <h3>How to Join</h3>
+          <p>If you’re interested in joining, hop on our discord using the link at the top of the page and apply directly.</p>
 
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
+          <h3>Code of Conduct</h3>
+          <p>
+            <ul>
+              <li>No seriously offensive language or bigotry.</li>
+              <li>Please avoid heavy or nsfw topics in the in-game clan chat.</li>
+              <li>No aggressive or condescending behaviour towards clanmates.</li>
+              <li>Respect other people’s boundaries, and avoid making others uncomfortable where possible.</li>
+              <li>If you’re uncertain of something, ask. We understand members will have varied levels of knowledge coming into the clan, and are here to help where needed.</li>
+            </ul>
+          </p>
          </div>
       </div>
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 import ParticlesBg  from "particles-bg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
@@ -7,19 +7,17 @@ class Header extends Component {
   render() {
 
     if(this.props.data){
-       var discord = this.props.data.discord;
-       var github = this.props.data.github;
+      var discord = this.props.data.discord;
+      var github = this.props.data.github;
       var name = this.props.data.name;
       var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
     }
 
     return (
       <header id="home">
-      <ParticlesBg type="circle" bg={true} />
+      <video playsInline autoPlay muted loop>
+        <source src="/Pride_Flag.webm" type="video/webm" />
+      </video>
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -27,9 +25,7 @@ class Header extends Component {
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+	          <li><a className="smoothscroll" href="#events">Events</a></li>
          </ul>
       </nav>
 
@@ -37,10 +33,10 @@ class Header extends Component {
       
          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
+            <h3>{description}</h3>
             <hr />
             <ul className="social">
-               <a href={discord} target="_blank" className="button btn project-btn"><FontAwesomeIcon icon={faDiscord} /> Join us on Discord!</a>
+               <a href={discord} target="_blank" className="button btn project-btn"><FontAwesomeIcon icon={faDiscord} />Join us on Discord!</a>
                <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Twitter</a>
             </ul>
          </div>
